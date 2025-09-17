74dc72ceab31832d04e5990c274a5c67542a5b82
dax <dax26d@gmail.com>
2025-09-17 12:06:55 +0800

    feat(ui): 添加历史记录侧边栏和HistoryListView组件
    在main window左侧添加dock widget用于显示历史记录，导入HistoryListView自定义组件并调整菜单栏高度。

--- deepseekchat.py
+++ deepseekchat.py
@@ -15,9 +15,11 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QMainWindow, QMenuBar,
-    QPushButton, QSizePolicy, QStatusBar, QTextEdit,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QDockWidget, QGridLayout, QMainWindow,
+    QMenuBar, QPushButton, QSizePolicy, QStatusBar,
+    QTextEdit, QVBoxLayout, QWidget)
+
+from historylistview import HistoryListView
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -66,11 +68,24 @@ class Ui_MainWindow(object):
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 1000, 24))
+        self.menubar.setGeometry(QRect(0, 0, 1000, 37))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
+        self.dockWidget_2 = QDockWidget(MainWindow)
+        self.dockWidget_2.setObjectName(u"dockWidget_2")
+        self.dockWidgetContents_2 = QWidget()
+        self.dockWidgetContents_2.setObjectName(u"dockWidgetContents_2")
+        self.verticalLayout_2 = QVBoxLayout(self.dockWidgetContents_2)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.listView = HistoryListView(self.dockWidgetContents_2)
+        self.listView.setObjectName(u"listView")
+
+        self.verticalLayout_2.addWidget(self.listView)
+
+        self.dockWidget_2.setWidget(self.dockWidgetContents_2)
+        MainWindow.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, self.dockWidget_2)
 
         self.retranslateUi(MainWindow)
 
