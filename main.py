6f5619551b30974990cbfbb58353f8dd6d6a2268
dax <dax26d@gmail.com>
2025-09-23 16:19:57 +0800

    feat(core): 添加Qt应用中文翻译支持

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ import sys
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 from mainwindow import MainWindow
+from PySide6.QtCore import QLibraryInfo, QTranslator
 
 
 def main():
     app = QApplication(sys.argv)
     app.setWindowIcon(QIcon(":/icons/icon_mail_chat.svg"))
     app.setApplicationName("DeepSeekChat")
+    translation_path = QLibraryInfo.path(QLibraryInfo.LibraryPath.TranslationsPath)
+    translator = QTranslator()
+    ok = translator.load('qtbase_zh_CN', translation_path)
+    if ok:
+        app.installTranslator(translator)
     win = MainWindow()
     win.show()
     sys.exit(app.exec())
