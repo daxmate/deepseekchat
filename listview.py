d3b5c70635d20411c3afa8ff49d5409063dc305d
dax <dax26d@gmail.com>
2025-09-23 17:03:31 +0800

    refactor(listview): 实现自定义ItemModel以增强数据管理
    - 新增ItemModel类继承QAbstractItemModel
    - 重构ListView类支持自定义模型
    - 添加add_items方法批量管理项目数据

--- listview.py
+++ listview.py
@@ -1,5 +1,31 @@
-from PySide6.QtWidgets import QListView
+from typing import Any
+
+from PySide6.QtWidgets import QListView, QMainWindow
+from PySide6.QtCore import QAbstractItemModel, Qt, QModelIndex
+
+
+class ItemModel(QAbstractItemModel):
+    def __init__(self, parent: QListView):
+        super().__init__(parent)
+        self.items = []
+
+    def rowCount(self, parent: QModelIndex = ...) -> int:
+        return len(self.items)
+
+    def data(self, index: QModelIndex, role: int = ...) -> Any:
+        if not index.isValid():
+            return None
+        item = self.items[index.row()]
+        if role == Qt.ItemDataRole.DisplayRole:
+            return item.data(index)
+        return None
+
 
 class ListView(QListView):
-    def __init__(self, parent=None):
+    def __init__(self, model: ItemModel = None, parent=None):
         super().__init__(parent)
+        self.model = model or ItemModel(self)
+
+    def add_items(self, items):
+        self.model.items.extend(items)
+        self.model.layoutChanged.emit()
