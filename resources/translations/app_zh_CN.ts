22420deb57601eb24f0dbb87aa8734ff409b16d1
dax <dax26d@gmail.com>
2025-09-25 16:32:17 +0800

    chore(i18n): 更新中文翻译文件

--- resources/translations/app_zh_CN.ts
+++ resources/translations/app_zh_CN.ts
@@ -3,61 +3,76 @@
 <TS version="2.1" language="zh_CN">
 <context>
     <name>DeepSeek</name>
-<message>
-<location filename="deepseek.py" line="49"/>
-    <source>You are a professional email reply assistant</source>
-<translation>您是专业的邮件回复助手</translation>
-</message>
-<message>
-<location filename="deepseek.py" line="75"/>
-    <source>Sending messages...</source>
-<translation>发送消息中...</translation>
-</message>
-<message>
-<location filename="deepseek.py" line="99"/>
-    <source>Reply email as follows:
+    <message>
+        <source>You are a professional email reply assistant</source>
+        <translation type="vanished">您是专业的邮件回复助手</translation>
+    </message>
+    <message>
+        <source>Sending messages...</source>
+        <translation type="vanished">发送消息中...</translation>
+    </message>
+    <message>
+        <source>Reply email as follows:
 </source>
-<translation>按以下方式回复邮件：
+        <translation type="vanished">按以下方式回复邮件：
 </translation>
-</message>
-<message>
-<location filename="deepseek.py" line="173"/>
-    <source>Error processing streaming response: {str(e)}</source>
-<translation>处理流式响应错误：</translation>
-</message>
+    </message>
+    <message>
+        <source>Error processing streaming response: {str(e)}</source>
+        <translation type="vanished">处理流式响应错误：</translation>
+    </message>
 </context>
 <context>
-<name>InputEditor</name>
-<message>
-<location filename="inputeditor.py" line="12"/>
-    <source>&quot;↑↓&quot; to navigate input history &quot;⇧↩&quot; to insert newline &quot;↩&quot; to send</source>
-<translation>&quot;↑↓&quot; 导航输入历史 &quot;⇧↩&quot; 插入换行 &quot;↩&quot; 发送</translation>
-</message>
+    <name>InputEditor</name>
+    <message>
+        <source>&quot;↑↓&quot; to navigate input history &quot;⇧↩&quot; to insert newline &quot;↩&quot; to send</source>
+        <translation type="vanished">&quot;↑↓&quot; 导航输入历史 &quot;⇧↩&quot; 插入换行 &quot;↩&quot; 发送</translation>
+    </message>
 </context>
 <context>
-<name>MainWindow</name>
-<message>
-<location filename="mainwindow.py" line="101"/>
-    <source>Please reply according to the original email and include the following information:
+    <name>MainWindow</name>
+    <message>
+        <source>Please reply according to the original email and include the following information:
     </source>
-    <translation>请按邮件原文回复并包含以下信息：
+        <translation type="vanished">请按邮件原文回复并包含以下信息：
 </translation>
-</message>
+    </message>
+    <message>
+        <location filename="../../src/forms/mainwindow.ui" line="14"/>
+        <source>DeepSeek邮件助手</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/forms/mainwindow.ui" line="65"/>
+        <source>&quot;↑↓&quot; to navigate input history &quot;⇧↩&quot; to insert newline &quot;↩&quot; to send</source>
+        <translation type="unfinished">&quot;↑↓&quot; 导航输入历史 &quot;⇧↩&quot; 插入换行 &quot;↩&quot; 发送</translation>
+    </message>
+    <message>
+        <location filename="../../src/forms/mainwindow.ui" line="90"/>
+        <source>发送</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
-<name>OutputTextEdit</name>
-<message>
-<location filename="outputtextedit.py" line="13"/>
-    <source>Thinking...</source>
-<translation>思考中...</translation>
-</message>
+    <name>OutputTextEdit</name>
+    <message>
+        <source>Thinking...</source>
+        <translation type="vanished">思考中...</translation>
+    </message>
 </context>
 <context>
-<name>Platform</name>
-<message>
-<location filename="platform.py" line="50"/>
-    <source>Deepseek API key not found. Please set it in the config file.</source>
-<translation>未找到Deepseek API密钥。请在配置文件中设置。</translation>
-</message>
+    <name>Platform</name>
+    <message>
+        <source>Deepseek API key not found. Please set it in the config file.</source>
+        <translation type="vanished">未找到Deepseek API密钥。请在配置文件中设置。</translation>
+    </message>
+</context>
+<context>
+    <name>Preferences</name>
+    <message>
+        <location filename="../../src/forms/preferences.ui" line="17"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 </TS>
